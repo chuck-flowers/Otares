a4308a29375f8487434675bf7d940a37f602e336
t_chu <t_chu@DESKTOP-N18R8JQ>
2016-07-25 20:18:02 -0400

    Initial Version 1.0

---
+++ Otares.NET/CrateModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Otares
+{
+    public class CrateModel
+    {
+        private const string CRATE_FIELD = "osrt";
+        
+        //CONSTRUCTOR
+
+        public CrateModel(string name, string content, OtaresDb context)
+        {
+            Name = name;
+
+            //Normalizes the input
+            int newStart = content.IndexOf(CRATE_FIELD);
+            content = content.Substring(newStart + CRATE_FIELD.Length);
+
+            //Breaks the input into two strings
+            int trackStart = content.IndexOf("otrk");
+
+            if (trackStart == -1)
+                return;
+
+            var columnString = content.Remove(trackStart);
+            var trackString = content.Substring(trackStart);
+
+            //Parse the columns
+            parseColumns();
+
+            //Parse the members of the crate
+            parseTracks(trackString, context);
+        }
+
+
+        //PUBLIC METHODS
+
+        public override string ToString() => Name;
+
+
+        //PUBLIC PROPERTIES
+
+        public string Name { get; set; }
+
+        public List<SongModel> Songs { get; } = new List<SongModel>();
+
+
+        //PRIVATE METHODS
+
+        private void parseTracks(string content, OtaresDb context)
+        {
+            //Gets each individual track
+            IEnumerable<string> tracks = content.Split(new string[] { "otrk" }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Removes the first 12 characters from each string
+            tracks = tracks.Select(str => str.Substring(12));
+
+            //Eliminates the blank characters between each UTF-8 string
+            tracks = tracks.Select(str =>
+            {
+                var newStr = "";
+                for (int i = 0; i < str.Length; i++)
+                    if (i % 2 == 1) newStr += str[i];
+                return newStr;
+            });
+
+            //Adds each of the specified files to the crate
+            foreach(var path in tracks)
+                Songs.Add(context.Songs.Find(s => s.Path == path));
+        }
+
+        private void parseColumns()
+        {
+
+        }
+    }
+}
---
+++ Otares.NET/SeratoDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Otares
+{
+    public class OtaresDb
+    {
+        //CONSTRUCTORS
+
+        public OtaresDb() : this(null) { }
+
+        public OtaresDb(string dbContent)
+        {
+            initDatabase(dbContent);
+        }
+        
+        
+        //PUBLIC PROPERTIES
+        
+        public List<SongModel> Songs { get; } = new List<SongModel>();
+
+        public List<CrateModel> Crates { get; } = new List<CrateModel>();
+
+
+        //PRIVATE METHODS
+
+        private void initDatabase(string content)
+        {
+            //Splits the content by entries
+            var entries = content.Split(new string[] { "otrk" }, StringSplitOptions.None);
+
+            //Builds a SongModel from each entry in the database
+            for (int i = 1; i < entries.Length; i++)
+                Songs.Add(new SongModel(entries[i]));
+
+            //TODO: Add support for crates
+        }
+    }
+}
